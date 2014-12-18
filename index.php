05dec84ff50c196fbf346b6425ab1b03243fce03
spoliv <spoliv@rambler.ru>
2014-12-18 16:15:55 +0300

    Commit 18.12.2014

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $view -> articles = [
     ['title' => 'Вторая новость', 'text' => 'Россия справится'],
     ['title' => 'Третья новость', 'text' => 'Всем злопыхателям назло'],
     ['title' => 'Новость дня', 'text' => 'Прессконференция Президента'],
-
+    ['title' => 'News of week', 'text' => 'President meeting'],
 ];
 $view -> display('index.php');
